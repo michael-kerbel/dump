396b73002e7f0fd1b820264e8b327e98fcbcb047
Michael <75951847+michael-kerbel@users.noreply.github.com>
2017-06-06 14:07:18 +0200

    added warning to JavaDoc: NonUniqueIndex.lookup() Iterables have to be synchronized by their user

--- src/util/dump/NonUniqueIndex.java
+++ src/util/dump/NonUniqueIndex.java
@@ -3,10 +3,25 @@ package util.dump;
 
 public interface NonUniqueIndex<E> {
 
-   public Iterable<E> lookup( int key );
+   /**
+    * BEWARE: While this method is synchronized, the iteration afterwards is not! You have to synchronize using
+    * the dump as monitor while iterating the Iterable, otherwise you might miss values in the iteration or get
+    * null values.
+    */
+   Iterable<E> lookup( int key );
 
-   public Iterable<E> lookup( long key );
+   /**
+    * BEWARE: While this method is synchronized, the iteration afterwards is not! You have to synchronize using
+    * the dump as monitor while iterating the Iterable, otherwise you might miss values in the iteration or get
+    * null values.
+    */
+   Iterable<E> lookup( long key );
 
-   public Iterable<E> lookup( Object key );
+   /**
+    * BEWARE: While this method is synchronized, the iteration afterwards is not! You have to synchronize using
+    * the dump as monitor while iterating the Iterable, otherwise you might miss values in the iteration or get
+    * null values.
+    */
+   Iterable<E> lookup( Object key );
 
 }
