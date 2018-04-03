3a166b65db941c06965d2d71cf198cbf646e08c7
Michael <75951847+michael-kerbel@users.noreply.github.com>
2018-04-03 18:20:39 +0200

    initial implementation of SearchIndex in new module util-dumpsearch

---
+++ dumpsearch/src/util/dump/SearchIndex.java
@@ -0,0 +1,296 @@
+package util.dump;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BiConsumer;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.classic.QueryParser.Operator;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import gnu.trove.list.TLongList;
+import util.reflection.FieldAccessor;
+import util.reflection.FieldFieldAccessor;
+import util.reflection.Reflection;
+
+
+/**
+ * <p>A DumpIndex allowing the dump entries to be searched for using a Lucene index.</p>
+ *
+ * <p>You need to provide a documentBuilder, which adds the Lucene index fields you want to search later.</p>
+ *
+ * <p><code>
+ *    SearchIndex<Bean> index = new SearchIndex<>(dump, new FieldFieldAccessor(field), ( doc, bean ) -> doc.add(new TextField("data", bean._data, Store.NO)));
+ * </code></p>
+ *
+ * <p>When searching you need to prefix your query tokens with the field you want to search in. A classic {@link QueryParser} is being used,
+ * with the internal "id" field and the AND operator as defaults.</p>
+ *
+ * <p><code>
+ *    for(Bean bean: index.search("data:searchtoken")) doSomething(bean);
+ * </code></p>
+ *
+ * <p>For custom Analyzers (default is {@link StandardAnalyzer}) use the {@link IndexWriterConfig} constructor param. You have to use the same config
+ * every time you use the index!</p>
+ */
+public class SearchIndex<E> extends DumpIndex<E> {
+
+   private IndexWriter             _writer;
+   private QueryParser             _parser;
+   private IndexSearcher           _searcher;
+   private BiConsumer<Document, E> _documentBuilder;
+   private IndexWriterConfig       _config;
+
+   private AtomicBoolean _commitIsPending = new AtomicBoolean(false);
+
+
+   public SearchIndex( Dump<E> dump, FieldAccessor idFieldAccessor, BiConsumer<Document, E> documentBuilder ) {
+      this(dump, idFieldAccessor, documentBuilder, null);
+   }
+
+   public SearchIndex( Dump<E> dump, FieldAccessor idFieldAccessor, BiConsumer<Document, E> documentBuilder, IndexWriterConfig config ) {
+
+      super(dump, idFieldAccessor, new File(dump.getDumpFile().getParentFile(), dump.getDumpFile().getName()+".search.index"));
+
+      _documentBuilder = documentBuilder;
+      _config = config;
+      if ( _config == null ) {
+         _config = new IndexWriterConfig(new StandardAnalyzer());
+      }
+
+      init();
+
+      _parser = new QueryParser("id", _config.getAnalyzer());
+      _parser.setDefaultOperator(Operator.AND);
+
+      openSearcher();
+   }
+
+   public SearchIndex( Dump<E> dump, String idFieldName, BiConsumer<Document, E> documentBuilder ) throws NoSuchFieldException {
+      this(dump, new FieldFieldAccessor(Reflection.getField(dump._beanClass, idFieldName)), documentBuilder);
+   }
+
+   @Override
+   public void close() throws IOException {
+      commit();
+      _writer.close();
+      _searcher.getIndexReader().close();
+      super.close();
+   }
+
+   /** Only provided to fulfill API requirements of parent class. You'll want to use {@link #countMatches(String)} or {@link #search(String)} most probably.
+    * @return true if an entry exists in the dump whose id field has the value provided */
+   @Override
+   public boolean contains( int id ) {
+      try {
+         return search("" + id, 1).iterator().hasNext();
+      }
+      catch ( ParseException | IOException e ) {
+         throw new RuntimeException("Failed to search", e);
+      }
+   }
+
+   /** Only provided to fulfill API requirements of parent class. You'll want to use {@link #countMatches(String)} or {@link #search(String)} most probably.
+    * @return true if an entry exists in the dump whose id field has the value provided */
+   @Override
+   public boolean contains( long id ) {
+      try {
+         return search("" + id, 1).iterator().hasNext();
+      }
+      catch ( ParseException | IOException e ) {
+         throw new RuntimeException("Failed to search", e);
+      }
+   }
+
+   /** Only provided to fulfill API requirements of parent class. You'll want to use {@link #countMatches(String)} or {@link #search(String)} most probably.
+    * @return true if an entry exists in the dump whose id field has the value provided */
+   @Override
+   public boolean contains( Object id ) {
+      try {
+         return search(id.toString(), 1).iterator().hasNext();
+      }
+      catch ( ParseException | IOException e ) {
+         throw new RuntimeException("Failed to search", e);
+      }
+   }
+
+   /**
+    * Counts the number of matches in the dump for the given Lucene query. This is way faster than searching and counting the results.
+    * @param query a valid Lucene Query, which will be parsed using the provided Analyzer or StandardAnalyzer, if none provided.
+    */
+   public int countMatches( String query ) throws ParseException, IOException {
+      return getSearcher().count(_parser.parse(query));
+   }
+
+   /**
+    * Unimplemented!
+    */
+   @Override
+   public TLongList getAllPositions() {
+      throw new UnsupportedOperationException();
+   }
+
+   @Override
+   public int getNumKeys() {
+      try {
+         return getSearcher().getIndexReader().numDocs();
+      }
+      catch ( IOException e ) {
+         throw new RuntimeException("Failed to query number of docs", e);
+      }
+   }
+
+   /**
+    * Searches for entries in the dump using the given Lucene query, loading all matching items lazily from the dump during iteration.
+    * @param query a valid Lucene Query, which will be parsed using the provided Analyzer or StandardAnalyzer, if none provided.
+    */
+   public Iterable<E> search( String query ) throws ParseException, IOException {
+      return search(query, getNumKeys());
+   }
+
+   /**
+    * Searches for entries in the dump using the given Lucene query, loading all matching items lazily from the dump during iteration.
+    * @param query a valid Lucene Query, which will be parsed using the provided Analyzer or StandardAnalyzer, if none provided.
+    * @param maxHits the maximum number of results to return
+    */
+   public Iterable<E> search( String query, int maxHits ) throws ParseException, IOException {
+      ScoreDoc[] docs = getSearcher().search(_parser.parse(query), Math.max(1, maxHits)).scoreDocs;
+      return () -> new Iterator<E>() {
+
+         int i = 0;
+
+
+         @Override
+         public boolean hasNext() {
+            return i < docs.length;
+         }
+
+         @Override
+         public E next() {
+            try {
+               Document doc = getSearcher().doc(docs[i].doc);
+               String pos = doc.get("pos");
+               E e = _dump.get(Long.parseLong(pos));
+               i++;
+               return e;
+            }
+            catch ( IOException e ) {
+               throw new RuntimeException("Failed to perform search", e);
+            }
+         }
+      };
+   }
+
+   protected void commit() throws IOException {
+      if ( _commitIsPending.get() ) {
+         _writer.commit();
+         _commitIsPending.set(false);
+      }
+   }
+
+   @Override
+   protected String getIndexType() {
+      return SearchIndex.class.getSimpleName();
+   }
+
+   protected IndexSearcher getSearcher() throws IOException {
+      commit();
+      DirectoryReader reader = DirectoryReader.openIfChanged((DirectoryReader)_searcher.getIndexReader(), _writer, false);
+      if ( reader != null ) {
+         _searcher = new IndexSearcher(reader);
+      }
+      return _searcher;
+   }
+
+   @Override
+   protected void initLookupMap() {
+      // does nothing
+   }
+
+   @Override
+   protected void initLookupOutputStream() {
+      try {
+         Directory dir = FSDirectory.open(getLookupFile().toPath());
+         _config.setOpenMode(OpenMode.CREATE_OR_APPEND);
+         _writer = new IndexWriter(dir, _config);
+         _writer.commit();
+      }
+      catch ( IOException e ) {
+         throw new RuntimeException("Failed to initialize dump index with lookup file " + getLookupFile(), e);
+      }
+   }
+
+   @Override
+   protected void load() {
+      // does nothing, we only need to open the Searcher
+   }
+
+   protected void openSearcher() {
+      try {
+         _searcher = new IndexSearcher(DirectoryReader.open(FSDirectory.open(getLookupFile().toPath())));
+      }
+      catch ( IOException e ) {
+         throw new RuntimeException("Failed to initialize dump index with lookup file " + getLookupFile(), e);
+      }
+   }
+
+   @Override
+   void add( E o, long pos ) {
+      try {
+         Document doc = new Document();
+         doc.add(new StringField("id", getId(o), Field.Store.YES));
+         doc.add(new StringField("pos", "" + pos, Field.Store.YES));
+         _documentBuilder.accept(doc, o);
+         _writer.addDocument(doc);
+         _commitIsPending.set(true);
+      }
+      catch ( IOException e ) {
+         throw new RuntimeException("Failed to add to index", e);
+      }
+   }
+
+   @Override
+   void delete( E o, long pos ) {
+      try {
+         _writer.deleteDocuments(_parser.parse(getId(o) + " pos:" + pos));
+         _commitIsPending.set(true);
+      }
+      catch ( IOException | ParseException e ) {
+         throw new RuntimeException("Failed to delete pos " + pos, e);
+      }
+   }
+
+   @Override
+   void update( long pos, E oldItem, E newItem ) {
+      try {
+         Document doc = new Document();
+         doc.add(new StringField("id", getId(newItem), Field.Store.YES));
+         doc.add(new StringField("pos", "" + pos, Field.Store.YES));
+         _documentBuilder.accept(doc, newItem);
+         _writer.updateDocument(new Term("pos", "" + pos), doc);
+         _commitIsPending.set(true);
+      }
+      catch ( IOException e ) {
+         throw new RuntimeException("Failed to update index", e);
+      }
+   }
+
+   private String getId( E o ) {
+      return (_fieldIsInt ? getIntKey(o) : (_fieldIsLong ? getLongKey(o) : getObjectKey(o))).toString();
+   }
+}
---
+++ dumpsearch/test/util/dump/SearchIndexTest.java
@@ -0,0 +1,187 @@
+package util.dump;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.TextField;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import util.io.IOUtils;
+import util.reflection.FieldFieldAccessor;
+import util.reflection.Reflection;
+
+
+public class SearchIndexTest {
+
+   private static File         _tmpdir;
+   private static final String DUMP_FILENAME = "DumpTest.dmp";
+
+
+   @BeforeClass
+   public static void setUpTmpdir() throws IOException {
+      _tmpdir = new File("target", "tmp");
+      _tmpdir.mkdirs();
+      if ( !_tmpdir.isDirectory() ) {
+         throw new IOException("unable to create temporary directory: " + _tmpdir.getAbsolutePath());
+      }
+      System.setProperty("java.io.tmpdir", _tmpdir.getAbsolutePath());
+   }
+
+   @Before
+   @After
+   public void deleteOldTestDumps() {
+      File[] dumpFile = _tmpdir.listFiles(f -> f.getName().startsWith("DumpTest."));
+      if ( dumpFile != null ) {
+         for ( File df : dumpFile ) {
+            if ( df.isDirectory() )
+               IOUtils.deleteDir(df);
+            else if ( !df.delete() ) {
+               System.out.println("Failed to delete old dump file " + df);
+            }
+         }
+      }
+   }
+
+   @Test
+   public void testBasicOperations() throws Exception {
+      File dumpFile = new File(_tmpdir, DUMP_FILENAME);
+
+      try (Dump<Bean> dump = new Dump<>(Bean.class, dumpFile)) {
+
+         Field field = Reflection.getField(Bean.class, "_idLong");
+         SearchIndex<Bean> index = new SearchIndex<>(dump, new FieldFieldAccessor(field), ( doc, o ) -> doc.add(new TextField("data", o._data, Store.NO)));
+
+         Bean firstBean = new Bean(1, "first  row");
+         dump.add(firstBean);
+         Bean secondBean = new Bean(2, "second row");
+         dump.add(secondBean);
+         Bean thirdBean = new Bean(3, "third one");
+         dump.add(thirdBean);
+
+         assertThat(index.contains(1)).as("contains does not work").isTrue();
+
+         List<Bean> beans = search(index, "data:first");
+         assertSingleResult(beans, firstBean);
+
+         beans = search(index, "data:second");
+         assertSingleResult(beans, secondBean);
+
+         beans = search(index, "data:row");
+         assertThat(beans.isEmpty()).as("query did not find items").isFalse();
+         assertThat(beans.size()).as("query did not find all items").isEqualTo(2);
+         assertThat(beans.contains(firstBean)).as("query found unexpected items").isTrue();
+         assertThat(beans.contains(secondBean)).as("query found unexpected items").isTrue();
+
+         assertThat(index.countMatches("data:row")).as("count did not find all items").isEqualTo(2);
+
+         beans = search(index, "data:third");
+         assertSingleResult(beans, thirdBean);
+         dump.deleteLast();
+
+         assertThat(index.getNumKeys()).as("deletion failed").isEqualTo(2);
+         beans = search(index, "data:third");
+         assertThat(beans).as("deletion failed").isEmpty();
+
+         beans = search(index, "data:first");
+         assertSingleResult(beans, firstBean);
+         Bean updatedFirstBean = beans.get(0);
+         updatedFirstBean._data = "fourth row";
+         dump.updateLast(updatedFirstBean);
+
+         beans = search(index, "data:fourth");
+         assertSingleResult(beans, updatedFirstBean);
+      }
+
+      try (Dump<Bean> dump = new Dump<>(Bean.class, dumpFile)) {
+
+         Field field = Reflection.getField(Bean.class, "_idLong");
+         SearchIndex<Bean> index = new SearchIndex<>(dump, new FieldFieldAccessor(field), ( doc, o ) -> {});
+
+         List<Bean> beans = search(index, "data:row");
+         assertThat(beans.isEmpty()).as("query did not find items").isFalse();
+         assertThat(beans.size()).as("query did not find all items").isEqualTo(2);
+      }
+   }
+
+   private void assertSingleResult( List<Bean> beans, Bean firstBean ) {
+      assertThat(beans.isEmpty()).as("query did not find item").isFalse();
+      assertThat(beans.size()).as("query found too many items").isEqualTo(1);
+      assertThat(beans.get(0)).as("query found wrong item").isEqualTo(firstBean);
+   }
+
+   private List<Bean> search( SearchIndex<Bean> index, String query ) throws org.apache.lucene.queryparser.classic.ParseException, IOException {
+      Iterable<Bean> result = index.search(query);
+      return StreamSupport.stream(result.spliterator(), false).collect(Collectors.toList());
+   }
+
+
+   public static class Bean implements ExternalizableBean {
+
+      @externalize(1)
+      long   _idLong;
+      @externalize(2)
+      int    _idInt;
+      @externalize(3)
+      String _idString;
+      @externalize(10)
+      String _data;
+
+
+      public Bean() {
+         // for Externalization
+      }
+
+      public Bean( int id, String data ) {
+         _idLong = id;
+         _idInt = id;
+         _idString = (id < 0 ? "" : "+") + id;
+         _data = data;
+      }
+
+      @Override
+      public boolean equals( Object obj ) {
+         if ( this == obj ) {
+            return true;
+         }
+         if ( obj == null ) {
+            return false;
+         }
+         if ( getClass() != obj.getClass() ) {
+            return false;
+         }
+         Bean other = (Bean)obj;
+         if ( _data == null ) {
+            if ( other._data != null ) {
+               return false;
+            }
+         } else if ( !_data.equals(other._data) ) {
+            return false;
+         }
+         if ( _idInt != other._idInt ) {
+            return false;
+         }
+         if ( _idLong != other._idLong ) {
+            return false;
+         }
+         if ( _idString == null ) {
+            return other._idString == null;
+         } else
+            return _idString.equals(other._idString);
+      }
+
+      @Override
+      public int hashCode() {
+         throw new UnsupportedOperationException("hashCode() not needed.");
+      }
+   }
+}
