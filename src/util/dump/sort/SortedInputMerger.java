b543a12c9e5ed6664dc3c42f12b1cc41dc782ef2
Michael <75951847+michael-kerbel@users.noreply.github.com>
2018-08-30 14:08:14 +0200

    better logging if stream is not sorted, code cleanup

--- src/util/dump/sort/SortedInputMerger.java
+++ src/util/dump/sort/SortedInputMerger.java
@@ -7,7 +7,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import util.dump.DumpInput;
+import util.dump.DumpReader;
 
 
 /**
@@ -17,44 +21,31 @@ import util.dump.DumpInput;
  */
 class SortedInputMerger<E> implements DumpInput<E>, Iterator<E> {
 
-   private DumpInput<E>  dualChannelA   = null;                    // first channel for dual stream mode (using comparator)
+   private DumpInput<E> dualChannelA = null; // first channel for dual stream mode (using comparator)
 
    private Iterator<E>   iteratorA      = null;
    private E             lastA          = (E)null;
-   private DumpInput<E>  dualChannelB   = null;                    // second channel for dual stream mode (using comparator)
+   private DumpInput<E>  dualChannelB   = null;                     // second channel for dual stream mode (using comparator)
    private Iterator<E>   iteratorB      = null;
    private E             lastB          = (E)null;
-   private Iterator<E>   singleIterator = null;                    // channel for single stream mode
+   private Iterator<E>   singleIterator = null;                     // channel for single stream mode
    private E             lastSingle     = (E)null;
-   private E             nextElement    = (E)null;                 // next element to be returned
+   private E             nextElement    = (E)null;                  // next element to be returned
    private boolean       nextPrepared   = false;
-   private Comparator<E> comparator     = null;                    // reference to the provided Comparator
+   private Comparator<E> comparator     = null;                     // reference to the provided Comparator
    private MergerMode    mergerMode     = MergerMode.uninitialized; // indicates the current merger status
 
 
    /**
-    * Constructs a merger using the given collection of <code>DumpInput</code> objetcs,
-    * the class will create internally a <code>Comparator</code> object based on the
-    * current implementation of <code>Comparable</code>
-    *
-    * @param inputstreams Colection (List) of <code>DumpInput</code> objects to be merged
-    * @throws Exception
-    */
-   public SortedInputMerger( List<DumpInput<E>> inputstreams ) throws Exception {
-      init(inputstreams, null);
-   }
-
-   /**
-    * Constructs a merger using the given collection of <code>DumpInput</code> objetcs,
+    * Constructs a merger using the given collection of <code>DumpInput</code> objects,
     * the class will merge the streams by using the provided Comparator
     *
-    * @param inputstreams Colection (List) of <code>DumpInput</code> objects to be merged
-    * @param comparator used to determine the item ordering
-    * @throws Exception
+    * @param inputStreams Collection (List) of <code>DumpInput</code> objects to be merged
+    * @param comparator used to determine the item ordering, will use natural ordering when null
     */
-   public SortedInputMerger( List<DumpInput<E>> inputstreams, Comparator<E> comparator ) throws Exception {
+   public SortedInputMerger( List<DumpInput<E>> inputStreams, @Nullable Comparator<E> comparator ) throws Exception {
       super();
-      init(inputstreams, comparator);
+      init(inputStreams, comparator);
    }
 
    /**
@@ -63,7 +54,7 @@ class SortedInputMerger<E> implements DumpInput<E>, Iterator<E> {
    @Override
    public void close() throws IOException {
 
-      this.nextElement = (E)null;
+      this.nextElement = null;
 
       if ( dualChannelA != null ) {
          dualChannelA.close();
@@ -72,21 +63,6 @@ class SortedInputMerger<E> implements DumpInput<E>, Iterator<E> {
       if ( dualChannelB != null ) {
          dualChannelB.close();
       }
-   }
-
-   private int compare( E a, E b ) {
-      if ( comparator == null ) {
-         if ( !(a instanceof Comparable) ) {
-            throw new IllegalArgumentException(a + " isn't Comparable and no comparator is set");
-         }
-         if ( !(b instanceof Comparable) ) {
-            throw new IllegalArgumentException(a + " isn't Comparable and no comparator is set");
-         }
-
-         return ((Comparable)a).compareTo(b);
-      }
-
-      return comparator.compare(a, b);
    }
 
    @Override
@@ -139,7 +115,7 @@ class SortedInputMerger<E> implements DumpInput<E>, Iterator<E> {
             }
             // EOF reached change status and inform the user
             mergerMode = MergerMode.concluded;
-            this.nextElement = (E)null;
+            this.nextElement = null;
             nextPrepared = true;
             return false;
 
@@ -152,25 +128,25 @@ class SortedInputMerger<E> implements DumpInput<E>, Iterator<E> {
             return true;
 
          case concluded:
-            this.nextElement = (E)null;
+            this.nextElement = null;
             nextPrepared = true;
             return false;
 
          case uninitialized:
-            throw new Exception("the SortedInputMerger is still uninitalized, this status is unexpected.");
+            throw new Exception("the SortedInputMerger is still uninitialized, this status is unexpected.");
 
          }
 
          throw new Exception("Unexpected merger status");
       }
-      catch ( Exception argh ) {
-         throw new RuntimeException(argh);
+      catch ( Exception e ) {
+         throw new RuntimeException(e);
       }
 
    }
 
    @Override
-   public Iterator<E> iterator() {
+   public @Nonnull Iterator<E> iterator() {
       return this;
    }
 
@@ -192,6 +168,22 @@ class SortedInputMerger<E> implements DumpInput<E>, Iterator<E> {
       super.finalize();
    }
 
+   private int compare( E a, E b ) {
+      if ( comparator == null ) {
+         if ( !(a instanceof Comparable) ) {
+            throw new IllegalArgumentException(a + " isn't Comparable and no comparator is set");
+         }
+         if ( !(b instanceof Comparable) ) {
+            throw new IllegalArgumentException(a + " isn't Comparable and no comparator is set");
+         }
+
+         //noinspection unchecked
+         return ((Comparable)a).compareTo(b);
+      }
+
+      return comparator.compare(a, b);
+   }
+
    // used to generate the initial merge channels A and B
    private DumpInput<E> getMergerForList( List<DumpInput<E>> sublist ) throws Exception {
 
@@ -199,43 +191,49 @@ class SortedInputMerger<E> implements DumpInput<E>, Iterator<E> {
 
       // list in empty, return a dummy NULL input stream
       if ( listSize == 0 ) {
-         return new ListInput<E>(new ArrayList<E>());
+         return new ListInput<>(new ArrayList<>());
       }
 
-      // single element, returns the elemnt itself (it supports already the expected interface)
+      // single element, returns the element itself (it supports already the expected interface)
       if ( listSize == 1 ) {
          return sublist.get(0);
       }
 
-      // recusrivelly splits the list into a new merger
-      return new SortedInputMerger<E>(sublist, this.comparator);
+      // recursively splits the list into a new merger
+      return new SortedInputMerger<>(sublist, this.comparator);
 
    }
 
+   @SuppressWarnings("Duplicates")
    private boolean hasNextA() {
       boolean hasNext = iteratorA.hasNext();
       if ( hasNext ) {
          E next = iteratorA.next();
          if ( lastA != null && compare(lastA, next) > 0 ) {
+            if ( dualChannelA instanceof DumpReader )
+               throw new IllegalStateException("underlying stream " + ((DumpReader)dualChannelA).getSourceFile() + " not sorted!");
             throw new IllegalStateException("underlying stream not sorted!");
          }
          lastA = next;
       } else {
-         lastA = (E)null;
+         lastA = null;
       }
       return hasNext;
    }
 
+   @SuppressWarnings("Duplicates")
    private boolean hasNextB() {
       boolean hasNext = iteratorB.hasNext();
       if ( hasNext ) {
          E next = iteratorB.next();
          if ( lastB != null && compare(lastB, next) > 0 ) {
+            if ( dualChannelB instanceof DumpReader )
+               throw new IllegalStateException("underlying stream " + ((DumpReader)dualChannelB).getSourceFile() + " not sorted!");
             throw new IllegalStateException("underlying stream not sorted!");
          }
          lastB = next;
       } else {
-         lastB = (E)null;
+         lastB = null;
       }
       return hasNext;
    }
@@ -245,24 +243,24 @@ class SortedInputMerger<E> implements DumpInput<E>, Iterator<E> {
       if ( hasNext ) {
          lastSingle = singleIterator.next();
       } else {
-         lastSingle = (E)null;
+         lastSingle = null;
       }
       return hasNext;
    }
 
-   private void init( List<DumpInput<E>> inputstreams, Comparator<E> comparator ) throws Exception {
+   private void init( List<DumpInput<E>> inputStreams, Comparator<E> comparator ) throws Exception {
 
       // retains a reference to the comparator
       this.comparator = comparator;
 
       // splits the streams, notice that the distribution of stream items
       // tends to send more elements to the dualChannelA.
-      int numberOfStreams = inputstreams.size();
+      int numberOfStreams = inputStreams.size();
       int streamsForChannelA = (int)Math.ceil((float)numberOfStreams / 2);
 
       // generates the two basic channels
-      dualChannelA = getMergerForList(inputstreams.subList(0, streamsForChannelA));
-      dualChannelB = getMergerForList(inputstreams.subList(streamsForChannelA, numberOfStreams));
+      dualChannelA = getMergerForList(inputStreams.subList(0, streamsForChannelA));
+      dualChannelB = getMergerForList(inputStreams.subList(streamsForChannelA, numberOfStreams));
 
       iteratorA = dualChannelA.iterator();
       iteratorB = dualChannelB.iterator();
@@ -289,16 +287,16 @@ class SortedInputMerger<E> implements DumpInput<E>, Iterator<E> {
    }
 
 
-   // private enumartion used to document the merger status
+   // private enumeration used to document the merger status
    private enum MergerMode {
       /*
        * uninitialized: initial status, channels are not yed assigned nor initialized
-       * dualstream: channelA and channelB are active and with elements ready to be comapred.
+       * dualStream: channelA and channelB are active and with elements ready to be compared.
        * transition: one of both channelA or channelB reached EOF, the other channel has still an element to be returned before continuing reading from it.
-       * singleStream: only one channel is active, normal interface opeartion according to DumpInput.
+       * singleStream: only one channel is active, normal interface operation according to DumpInput.
        * concluded: no more data left, channels are empty.
        */
-      uninitialized, dualStream, transition, singleStream, concluded;
+      uninitialized, dualStream, transition, singleStream, concluded
    }
 
 }
