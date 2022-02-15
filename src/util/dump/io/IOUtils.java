edb8119ba636ecad798ad2e97f921b58b12fa505
Andreas Hager <andreas@chrono24.com>
2022-02-15 18:14:35 +0100

    Remove methods that are not used by dump framework, remove experimental MultithreadedDumpReader

--- src/util/dump/io/IOUtils.java
+++ src/util/dump/io/IOUtils.java
@@ -29,104 +29,6 @@ public class IOUtils {
    }
 
    /**
-    * Copies a single file to another location. Overwrites existing targets.
-    * Implementation is native and memory efficient.
-    */
-   public static void copy( File source, File dest ) throws IOException {
-      if ( Boolean.getBoolean("mentasys.util.io.noNIO") ) {
-         copyOldIO(source, dest);
-         return;
-      }
-
-      FileChannel in = null, out = null;
-      try {
-         in = new FileInputStream(source).getChannel();
-         out = new FileOutputStream(dest).getChannel();
-
-         long size = in.size();
-         long pos = 0;
-         while ( pos < size ) {
-            pos += in.transferTo(pos, size, out);
-         }
-      }
-      finally {
-         if ( in != null ) {
-            in.close();
-         }
-         if ( out != null ) {
-            out.close();
-         }
-      }
-   }
-
-   /**
-    * Copies a directory to another location. Overwrites existing targets. Doesn't delete the contents of destDir, if any.
-    * Implementation is native and memory efficient.
-    */
-   public static void copyDir( File sourceDir, File destDir ) throws IOException {
-      if ( !sourceDir.isDirectory() ) {
-         return;
-      }
-      if ( destDir.exists() && !destDir.isDirectory() ) {
-         return;
-      }
-      if ( !destDir.exists() ) {
-         destDir.mkdirs();
-      }
-      File[] files = sourceDir.listFiles();
-      for ( int i = 0; files != null && i < files.length; i++ ) {
-         if ( files[i].isDirectory() ) {
-            copyDir(files[i], new File(destDir, files[i].getName()));
-         } else {
-            copy(files[i], new File(destDir, files[i].getName()));
-         }
-      }
-   }
-
-   public static void copyDirOldIO( File sourceDir, File destDir ) throws IOException {
-      copyDirOldIO(sourceDir, destDir, null);
-   }
-
-   /**
-    * Copies a directory to another location. Overwrites existing targets. Doesn't delete the contents of destDir, if any.
-    * Implementation is native and memory efficient.
-    */
-   public static void copyDirOldIO( File sourceDir, File destDir, Pattern exclude ) throws IOException {
-      if ( !sourceDir.isDirectory() ) {
-         return;
-      }
-      if ( destDir.exists() && !destDir.isDirectory() ) {
-         return;
-      }
-      if ( !destDir.exists() ) {
-         destDir.mkdirs();
-      }
-      File[] files = sourceDir.listFiles();
-      for ( int i = 0; files != null && i < files.length; i++ ) {
-         if ( exclude != null && exclude.matcher(files[i].getName()).find() ) {
-            continue;
-         }
-         if ( files[i].isDirectory() ) {
-            copyDirOldIO(files[i], new File(destDir, files[i].getName()), exclude);
-         } else {
-            copyOldIO(files[i], new File(destDir, files[i].getName()));
-         }
-      }
-   }
-
-   public static void copyOldIO( File in, File out ) throws IOException {
-      FileInputStream fis = new FileInputStream(in);
-      FileOutputStream fos = new FileOutputStream(out);
-      byte[] buf = new byte[4096];
-      int i = 0;
-      while ( (i = fis.read(buf)) != -1 ) {
-         fos.write(buf, 0, i);
-      }
-      fis.close();
-      fos.close();
-   }
-
-   /**
     * Deletes a directory and its content recursively.
     * If <code>file</code> is not a directory, it is deleted. If <code>file</code> does not exist, nothing happens.
     */
@@ -144,22 +46,6 @@ public class IOUtils {
    }
 
    /**
-    * Deletes the content of a directory recursively, but lets the directory itself untouched.
-    * If <code>dir</code> does not exist, nothing happens.
-    */
-   public static void deleteDirContent( File dir ) {
-      if ( !dir.exists() ) {
-         return;
-      }
-      if ( dir.isDirectory() ) {
-         File[] files = dir.listFiles();
-         for ( int i = 0; files != null && i < files.length; i++ ) {
-            deleteDir(files[i]);
-         }
-      }
-   }
-
-   /**
     * Quietly get the canonical file (i.e. throw a {@link RuntimeException} instead of the {@link IOException})
     */
    public static File getCanonicalFileQuietly( File file ) {
@@ -169,37 +55,6 @@ public class IOUtils {
       catch ( IOException e ) {
          throw new RuntimeException("failed to resolve canonical file for " + file, e);
       }
-   }
-
-   /**
-    * Quietly get the canonical pathname of the file (i.e. throw a {@link RuntimeException} instead of the {@link IOException})
-    */
-   public static String getCanonicalPathQuietly( File file ) {
-      try {
-         return file.getCanonicalPath();
-      }
-      catch ( IOException e ) {
-         throw new RuntimeException("failed to resolve canonical path of " + file, e);
-      }
-   }
-
-   /**
-    * <p>ensures the directory to be existing</p>
-    * try's to create the directory (and parents) if they don't exist yet
-    * @throws IOException if the creation failed
-    */
-   public static void makeDirsIfNotExist( File directory ) throws IOException {
-      if ( directory == null ) throw new IllegalArgumentException("directory must not be null");
-
-      if ( directory.exists() ) {
-         if ( directory.isDirectory() ) {
-            return;
-         } else {
-            throw new IOException(directory + " already exists but is no directory");
-         }
-      }
-      directory.mkdirs(); // ignore it
-      if ( !directory.exists() || !directory.isDirectory() ) throw new IOException("unable to create directory (or parents): " + directory);
    }
 
    public static String readAsString( File file ) throws IOException {
@@ -212,10 +67,10 @@ public class IOUtils {
 
    public static String readAsString( InputStream in, String encoding ) throws IOException {
       BufferedReader reader = null;
-      StringBuffer sb;
+      StringBuilder sb;
       try {
          reader = new BufferedReader(new InputStreamReader(in, encoding));
-         sb = new StringBuffer();
+         sb = new StringBuilder();
          String line = null;
          while ( (line = reader.readLine()) != null ) {
             sb.append(line).append('\n');
@@ -231,10 +86,6 @@ public class IOUtils {
       }
 
       return sb.toString();
-   }
-
-   public static String readAsString( String filename ) throws IOException {
-      return readAsString(new FileInputStream(filename));
    }
 
    private static void closeCloseable( Closeable c ) {
--- src/util/dump/time/TimeUtils.java
+++ src/util/dump/time/TimeUtils.java
@@ -8,142 +8,16 @@ import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 
-public class TimeUtils {
+class TimeUtils {
 
    public static final long             SECOND_IN_MILLIS   = 1000L;
    public static final long             MINUTE_IN_MILLIS   = 60 * SECOND_IN_MILLIS;
    public static final long             HOUR_IN_MILLIS     = 60 * MINUTE_IN_MILLIS;
    public static final long             DAY_IN_MILLIS      = 24 * HOUR_IN_MILLIS;
-   public static final long             WEEKS_IN_MILLIS    = 7 * DAY_IN_MILLIS;
-   public static final long             MONTHS_IN_MILLIS   = 30 * DAY_IN_MILLIS;
-   public static final long             QUARTERS_IN_MILLIS = 91 * DAY_IN_MILLIS;
-   public static final long             YEARS_IN_MILLIS    = 365 * DAY_IN_MILLIS;
 
-   private static DecimalFormatSymbols  symbols            = new DecimalFormatSymbols(Locale.ENGLISH);
-   private static DecimalFormat         dual               = new DecimalFormat("00", symbols);
-   private static DecimalFormat         sf                 = new DecimalFormat("00.0##", symbols);
-
-   private static ThreadLocal<Calendar> CALENDAR           = new ThreadLocal<Calendar>() {
-
-                                                              @Override
-                                                              protected Calendar initialValue() {
-                                                                 return Calendar.getInstance();
-                                                              }
-                                                           };
-
-
-   /**
-    * checks if a Date is older than the number of days
-    *
-    * Example:
-    * <pre>
-    * Date oneWeekAgo = …
-    * dateOlderThanDays(oneWeekAgo, 3)  → true
-    * dateOlderThanDays(oneWeekAgo, 8)  → false
-    * </pre>
-    */
-   public static boolean dateOlderThanDays( Date date, long days ) {
-      long now = System.currentTimeMillis();
-      if ( date.getTime() < now - days * DAY_IN_MILLIS ) {
-         return true;
-      } else {
-         return false;
-      }
-   }
-
-   public static int getCalendarFieldValue( Date date, int calendarField ) {
-      Calendar calendar = CALENDAR.get();
-      calendar.setTime(date);
-      return calendar.get(calendarField);
-   }
-
-   /**
-    * <p>Returns the date that is rounded to a daily basis<p>
-    *
-    * Example:<br/>
-    *   roundDateToFullDay for the Date "2009-04-22 17:43:13" would return "2009-04-22 00:00:00"
-    */
-   public static Date roundDateToFullDay( final Date originalDate ) {
-      Calendar calendar = CALENDAR.get();
-      calendar.setTime(roundDateToFullHour(originalDate));
-      calendar.set(Calendar.HOUR_OF_DAY, 0);
-      return calendar.getTime();
-   }
-
-   /**
-    * <p>Returns the date that is rounded to a daily basis<p>
-    *
-    * Example:<br/>
-    *   roundDateToFullDay for the Date "2009-04-22 17:43:13" would return "2009-04-22 17:00:00"
-    */
-   public static Date roundDateToFullHour( final Date originalDate ) {
-      Calendar calendar = CALENDAR.get();
-      calendar.setTime(originalDate);
-      calendar.set(Calendar.MINUTE, 0);
-      calendar.set(Calendar.SECOND, 0);
-      calendar.set(Calendar.MILLISECOND, 0);
-      return calendar.getTime();
-   }
-
-   /**
-    * <p>Returns the date that is rounded to a monthly basis<p>
-    *
-    * Example:<br/>
-    *   roundDateToFullMonth for the Date "2009-04-22 17:43:13" would return "2009-04-01 00:00:00"
-    */
-   public static Date roundDateToFullMonth( final Date originalDate ) {
-      Calendar calendar = CALENDAR.get();
-      calendar.setTime(roundDateToFullDay(originalDate));
-      calendar.set(Calendar.DAY_OF_MONTH, 1);
-      return calendar.getTime();
-   }
-
-   /**
-    * <p>Returns the date that is rounded to a weekly basis. Beginning of the week is <b>Monday</b>.<p>
-    *
-    * Example:<br/>
-    *   roundDateToFullWeek for the Date "2009-04-22 17:43:13" would return "2009-04-20 00:00:00"
-    */
-   public static Date roundDateToFullWeek( final Date originalDate ) {
-      Calendar calendar = CALENDAR.get();
-      calendar.setTime(roundDateToFullDay(originalDate));
-      calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-      return calendar.getTime();
-   }
-
-   /**
-    * <p>calls Thread.sleep but immediately returns on an {@link InterruptedException} though it makes sure the interrupted status is saved</p>
-    * see <a href="http://www.javaspecialists.eu/archive/Issue146.html">The Law of the Sabotaged Doorbell</a>
-    *
-    * <p><b>Note:</b> There's no guarantee of how long the thread will actually sleep.
-    * Tests even showed, that sleepQuietly(100) might sleep more than <b>300 ms</b> if there's some load on the system!</p>
-    *
-    * @param milliseconds   time to sleep in milliseconds
-    */
-   public static void sleepQuietly( long milliseconds ) {
-      try {
-         TimeUnit.MILLISECONDS.sleep(milliseconds);
-      }
-      /**
-       * <quote>
-       * Note that the interrupted status is set to false when an InterruptedException is caused
-       * or when the Thread.interrupt method is explicitly called.
-       * Thus, when we catch an InterruptedException, we need to remember that the thread is now not interrupted anymore!
-       * In order to have orderly shutdown of the thread, we should keep the thread set to "interrupted".
-       * </quote>
-       */
-      catch ( InterruptedException e ) {
-         Thread.currentThread().interrupt();
-      }
-   }
-
-   /**
-    * @see #sleepQuietly(long)
-    * @param seconds    time to sleep in seconds
-    */
-   public static void sleepQuietlySeconds( long seconds ) {
-      sleepQuietly(seconds * SECOND_IN_MILLIS);
-   }
+   private static final DecimalFormatSymbols  symbols            = new DecimalFormatSymbols(Locale.ENGLISH);
+   private static final DecimalFormat         dual               = new DecimalFormat("00", symbols);
+   private static final DecimalFormat         sf                 = new DecimalFormat("00.0##", symbols);
 
    /**
     * writes an interval in a nice human readable format.
@@ -155,7 +29,7 @@ public class TimeUtils {
     */
    public static String toHumanReadableFormat( long milliseconds ) {
 
-      StringBuffer sb = new StringBuffer(16);
+      StringBuilder sb = new StringBuilder(16);
 
       if ( milliseconds < 0 ) {
          sb.append("-");
@@ -206,10 +80,4 @@ public class TimeUtils {
       return sb.toString();
    }
 
-   /**
-    * @see #toHumanReadableFormat(long)
-    */
-   public static String toHumanReadableFormatNano( long nanoseconds ) {
-      return toHumanReadableFormat(TimeUnit.NANOSECONDS.toMillis(nanoseconds));
-   }
 }
