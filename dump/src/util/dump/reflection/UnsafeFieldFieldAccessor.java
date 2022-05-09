aa42f136fa3974cea858f51b943f7f5f55f9d935
Christian Trefzer <christian.trefzer@chrono24.com>
2022-05-09 16:24:16 +0200

    BD-100: move identical classes in "util" to package matching structure in "dump", drop duplicate dependencies

--- dump/src/util/dump/reflection/UnsafeFieldFieldAccessor.java
+++ dump/src/util/dump/reflection/UnsafeFieldFieldAccessor.java
@@ -1,9 +1,9 @@
 package util.dump.reflection;
 
+import java.lang.reflect.Field;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Field;
 
 
 public class UnsafeFieldFieldAccessor extends FieldFieldAccessor {
